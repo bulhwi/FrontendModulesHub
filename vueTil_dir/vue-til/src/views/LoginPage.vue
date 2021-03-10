261137a51d8a2d8073de0f409035d778d51b4b47
bulhwi <mm225012@gmail.com>
2021-03-11 00:41:52 +0900

    로그인 api 연동

--- vueTil_dir/vue-til/src/views/LoginPage.vue
+++ vueTil_dir/vue-til/src/views/LoginPage.vue
@@ -1,11 +1,16 @@
 <template>
 	<div>
-		login
+		<h1>로그인 페이지</h1>
+		<LoginForm></LoginForm>
 	</div>
 </template>
 <script>
+import LoginForm from '@/components/LoginForm.vue';
 export default {
 	name: 'LoginPage',
+	components: {
+		LoginForm,
+	},
 };
 </script>
 
