3fff67dd44d48ba9e88722be31e9d3ca9cbdca1d
bulhwi <mm225012@gmail.com>
2021-05-10 22:40:53 +0900

    create post api

---
+++ vueTil_dir/vue-til/src/views/PostAddPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="form-container">
+		<PostAddForm></PostAddForm>
+	</div>
+</template>
+
+<script>
+import PostAddForm from '@/components/posts/PostAddForm.vue';
+export default {
+	name: 'PostAddPage',
+	components: {
+		PostAddForm,
+	},
+};
+</script>
+
+<style scoped></style>
